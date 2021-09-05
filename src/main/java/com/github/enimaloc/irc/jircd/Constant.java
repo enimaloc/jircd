dc879dcc81a136ed2b9e58822eb29d4222a11029
enimaloc <antoine@enimaloc.fr>
2021-09-05 22:57:00 +0200

    refactor: update VERSION constant to 1.0.0

--- src/main/java/com/github/enimaloc/irc/jircd/Constant.java
+++ src/main/java/com/github/enimaloc/irc/jircd/Constant.java
@@ -3,6 +3,6 @@ package com.github.enimaloc.irc.jircd;
 public class Constant {
 
     public static final String NAME    = "jircd";
-    public static final String VERSION = "0.0.0";
+    public static final String VERSION = "1.0.0";
     public static final String GITHUB  = "https://github.com/enimaloc/jircd";
 }
