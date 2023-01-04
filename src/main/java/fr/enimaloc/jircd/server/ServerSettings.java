97dc3a00e78a124f8f6b3e0d7faffe1ac1ed5e4c
enimaloc <antoine@enimaloc.fr>
2023-01-04 17:05:44 +0100

    feat(settings): add SSL parts in settings

--- src/main/java/fr/enimaloc/jircd/server/ServerSettings.java
+++ src/main/java/fr/enimaloc/jircd/server/ServerSettings.java
@@ -4,6 +4,10 @@ import com.electronwill.nightconfig.core.conversion.ObjectConverter;
 import com.electronwill.nightconfig.core.file.FileConfig;
 import com.electronwill.nightconfig.core.io.WritingException;
 import fr.enimaloc.jircd.message.Mask;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
@@ -13,9 +17,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.function.Predicate;
-import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ServerSettings {
 
@@ -26,6 +27,7 @@ public class ServerSettings {
     private           long           timeout;
     private           String         pass;
     private           String         host;
+    private           SSL            ssl;
     private           String         networkName;
     private           String         description;
     private           Admin          admin;
@@ -36,18 +38,19 @@ public class ServerSettings {
     private transient String[]       motd;
 
     ServerSettings(Builder builder) {
-        this.port           = builder.port;
-        this.pingTimeout    = builder.pingTimeout;
-        this.timeout        = builder.timeout;
-        this.pass           = builder.pass;
-        this.host           = builder.host;
-        this.networkName    = builder.networkName;
-        this.description    = builder.description;
-        this.admin          = builder.admin;
-        this.operators      = builder.operators;
+        this.port = builder.port;
+        this.pingTimeout = builder.pingTimeout;
+        this.timeout = builder.timeout;
+        this.pass = builder.pass;
+        this.host = builder.host;
+        this.ssl = builder.ssl;
+        this.networkName = builder.networkName;
+        this.description = builder.description;
+        this.admin = builder.admin;
+        this.operators = builder.operators;
         this.unsafeNickname = builder.unsafeNickname;
-        this.safeNet        = builder.safeNet;
-        this.motd           = builder.motd;
+        this.safeNet = builder.safeNet;
+        this.motd = builder.motd;
     }
 
     public ServerSettings() {
@@ -64,16 +67,16 @@ public class ServerSettings {
 
     public ServerSettings copy(ServerSettings to, Predicate<Field> copyIf) {
         Arrays.stream(this.getClass().getDeclaredFields())
-              .filter(copyIf.and(field -> !Modifier.isFinal(field.getModifiers())))
-              .forEach(field -> {
-                  if (field.trySetAccessible()) {
-                      try {
-                          field.set(to, field.get(this));
-                      } catch (IllegalAccessException e) {
-                          logger.error(e.getLocalizedMessage(), e);
-                      }
-                  }
-              });
+                .filter(copyIf.and(field -> !Modifier.isFinal(field.getModifiers())))
+                .forEach(field -> {
+                    if (field.trySetAccessible()) {
+                        try {
+                            field.set(to, field.get(this));
+                        } catch (IllegalAccessException e) {
+                            logger.error(e.getLocalizedMessage(), e);
+                        }
+                    }
+                });
         return to;
     }
 
@@ -111,17 +114,18 @@ public class ServerSettings {
         ServerSettings that = (ServerSettings) o;
 
         return port == that.port
-               && pingTimeout == that.pingTimeout
-               && timeout == that.timeout
-               && Objects.equals(pass, that.pass)
-               && Objects.equals(host(), that.host())
-               && Objects.equals(networkName, that.networkName)
-               && Objects.equals(description, that.description)
-               && Objects.equals(admin, that.admin)
-               && Objects.equals(operators, that.operators)
-               && Objects.equals(unsafeNickname, that.unsafeNickname)
-               && Objects.equals(safeNet, that.safeNet)
-               && Arrays.equals(motd, that.motd);
+                && pingTimeout == that.pingTimeout
+                && timeout == that.timeout
+                && Objects.equals(pass, that.pass)
+                && Objects.equals(host(), that.host())
+                && Objects.equals(ssl, that.ssl)
+                && Objects.equals(networkName, that.networkName)
+                && Objects.equals(description, that.description)
+                && Objects.equals(admin, that.admin)
+                && Objects.equals(operators, that.operators)
+                && Objects.equals(unsafeNickname, that.unsafeNickname)
+                && Objects.equals(safeNet, that.safeNet)
+                && Arrays.equals(motd, that.motd);
     }
 
     @Override
@@ -131,6 +135,7 @@ public class ServerSettings {
         result = 31 * result + (int) (timeout ^ (timeout >>> 32));
         result = 31 * result + Objects.hashCode(pass);
         result = 31 * result + Objects.hashCode(host());
+        result = 31 * result + Objects.hashCode(ssl);
         result = 31 * result + Objects.hashCode(networkName);
         result = 31 * result + Objects.hashCode(description);
         result = 31 * result + Objects.hashCode(admin);
@@ -163,10 +168,14 @@ public class ServerSettings {
                 host = InetAddress.getLocalHost().getHostAddress();
             } catch (UnknownHostException e) {
                 LoggerFactory.getLogger(ServerSettings.class)
-                             .error(e.getLocalizedMessage(), e);
+                        .error(e.getLocalizedMessage(), e);
             }
         }
         return host;
+    }
+
+    public Optional<SSL> ssl() {
+        return ssl.enabled() ? Optional.of(ssl) : Optional.empty();
     }
 
     public String networkName() {
@@ -200,19 +209,54 @@ public class ServerSettings {
     @Override
     public String toString() {
         return "ServerSettings{" +
-               "port=" + port +
-               ", pingTimeout=" + pingTimeout +
-               ", timeout=" + timeout +
-               ", pass='" + pass + '\'' +
-               ", host='" + host + '\'' +
-               ", networkName='" + networkName + '\'' +
-               ", description='" + description + '\'' +
-               ", admin=" + admin +
-               ", operators=" + operators +
-               ", unsafeNickname=" + unsafeNickname +
-               ", safeNet=" + safeNet +
-               ", motd=" + Arrays.toString(motd) +
-               '}';
+                "port=" + port +
+                ", pingTimeout=" + pingTimeout +
+                ", timeout=" + timeout +
+                ", pass='" + pass + '\'' +
+                ", host='" + host + '\'' +
+                ", ssl=" + ssl +
+                ", networkName='" + networkName + '\'' +
+                ", description='" + description + '\'' +
+                ", admin=" + admin +
+                ", operators=" + operators +
+                ", unsafeNickname=" + unsafeNickname +
+                ", safeNet=" + safeNet +
+                ", motd=" + Arrays.toString(motd) +
+                '}';
+    }
+
+    public static class SSL {
+        private boolean enabled;
+        private int     port;
+
+        public SSL() {
+            this(true, 6697);
+        }
+
+        public SSL(boolean enabled, int port) {
+            this.enabled = enabled;
+            this.port = port;
+        }
+
+        public boolean enabled() {
+            return enabled;
+        }
+
+        public void enabled(boolean enabled) {
+            this.enabled = enabled;
+        }
+
+        public int port() {
+            return port;
+        }
+
+        @Override
+        public String toString() {
+            return "SSL{" +
+                    "enabled=" + enabled +
+                    ", port=" + port +
+                    '}';
+        }
     }
 
     public static class Operator {
@@ -225,7 +269,7 @@ public class ServerSettings {
 
         public Operator(String username, String host, String password) {
             this.username = username;
-            this.host     = host;
+            this.host = host;
             this.password = password;
         }
 
@@ -256,8 +300,8 @@ public class ServerSettings {
         }
 
         public Admin(String loc1, String loc2, String email) {
-            this.loc1  = loc1;
-            this.loc2  = loc2;
+            this.loc1 = loc1;
+            this.loc2 = loc2;
             this.email = email;
         }
 
@@ -309,6 +353,7 @@ public class ServerSettings {
         private List<String>                  safeNet        = new ArrayList<>(
                 List.of("::1", "127.0.0.1", "localhost"));
         private String                        host;
+        private SSL                           ssl            = new SSL();
         private String[]                      motd           = new String[0];
 
         public Builder motdFile(Path motdFile) {
@@ -376,6 +421,11 @@ public class ServerSettings {
             return this;
         }
 
+        public Builder ssl(SSL ssl) {
+            this.ssl = ssl;
+            return this;
+        }
+
         public Builder motd(@Nullable String[] motd) {
             this.motd = motd;
             return this;
@@ -387,7 +437,7 @@ public class ServerSettings {
                     motd = Files.exists(motdFile) ? Files.readAllLines(motdFile).toArray(String[]::new) : new String[0];
                 } catch (IOException e) {
                     LoggerFactory.getLogger(ServerSettings.class)
-                                 .error(e.getLocalizedMessage(), e);
+                            .error(e.getLocalizedMessage(), e);
                 }
             }
             return new ServerSettings(this);
